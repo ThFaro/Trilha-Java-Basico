d82a3f547ded5a1cd803dc0e3b8436f80a65d7dc
Thiago Faro <thfaro.ads@gmail.com>
2023-07-24 12:52:17 -0300

    DESAFIO

--- trilha-java-basico/PilaresPoo/PilaresPoo/src/Desafio Iphone/Iphone/src/Funcoes/MultiTarefa/multiTarefas.java
+++ trilha-java-basico/PilaresPoo/PilaresPoo/src/Desafio Iphone/Iphone/src/Funcoes/MultiTarefa/multiTarefas.java
@@ -10,30 +10,30 @@ import Funcoes.Telefone.telefone;
 public class multiTarefas implements internet, telefone, player {
 
     public void exibirPagina(){
-        System.out.println("- EXIBINDO PÁGINA ATUAL");
+        System.out.println("- PÁGINA ATUAL");
     }
     public void adicionarNovaAba(){
-        System.out.println("- ADICIONANDO NOVA ABA");
+        System.out.println("- NOVA ABA");
     }
     public void atualizarPagina(){
         System.out.println("- ATUALIZANDO PÁGINA");
     }
 
     public void ligar(){
-        System.out.println("- REALIZANDO CHAMADA");
+        System.out.println("- DISCANDO");
     }
     public void atender(){
-        System.out.println("- ATENDENDO CHAMADA");
+        System.out.println("- RECEBENDO CHAMADA");
     }
     public void iniciarCorreioVoz(){
-        System.out.println("- ACESSANDO CORREIO DE VOZ");
+        System.out.println("- DISCANDO CORREIO DE VOZ");
     }
 
     public void tocar(){
         System.out.println("- TOCANDO MÚSICA");
     }
     public void pausar(){
-        System.out.println("- MÚSICA PAUSADA");
+        System.out.println("- PAUSADO");
     }
     public void selecionarMusica(){
         System.out.println("- MÚSICA SELECIONADA");
